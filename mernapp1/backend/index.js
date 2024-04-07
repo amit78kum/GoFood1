0b9f1d3116a4b12dea328bbf1d89b605e18d1692
amit78kum <amkumar7488@gmail.com>
2024-04-07 10:55:28 +0530

    branch1

--- mernapp1/backend/index.js
+++ mernapp1/backend/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   });
   
 app.get('/',(req,res)=>{
-    res.send('Hello World')
+    res.send('Hell World')
 })
 app.use(express.json());
 app.use('/api',require("./Routes/CreatUser"));
